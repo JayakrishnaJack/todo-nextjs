babe288838a35428dae91a90df88a7e482aa4d57
JayakrishnaJack <jayakrishna.s@openturf.in>
2024-08-03 19:04:12 +0530

    added: basic create and list todo items

---
+++ src/app/new/page.tsx
@@ -0,0 +1,45 @@
+import { prisma } from "@/db";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+
+async function addTodo(data: FormData) {
+  "use server";
+  const title = data.get("title")?.valueOf();
+  if (typeof title !== "string" || title.length === 0) {
+    throw new Error("Invalid Title");
+  }
+  await prisma.todo.create({ data: { title, complete: false } });
+  redirect("/");
+}
+
+function NewTodo() {
+  return (
+    <>
+      <h1 className="font-semibold text-xl">Add New Todo</h1>
+      <form className="max-w-lg" action={addTodo}>
+        <input
+          name="title"
+          type="text"
+          className="w-full m-4 rounded p-2 border-gray-200 text-slate-900 font-medium"
+        />
+        <br />
+        <div className="flex justify-end gap-2">
+          <Link
+            href="/"
+            className="w-28 h-8 rounded border border-red-500 text-center p-0.5 text-red-500"
+          >
+            Cancel
+          </Link>
+          <button
+            type="submit"
+            className="w-28 h-8 rounded border border-gray-200"
+          >
+            Add
+          </button>
+        </div>
+      </form>
+    </>
+  );
+}
+
+export default NewTodo;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
-export default function Home() {
+import { prisma } from "@/db";
+import Link from "next/link";
+
+export default async function Home() {
+  const res = await prisma.todo.findMany({});
+
   return (
-    <>
-      <h1>Todo App</h1>
-    </>
+    <div>
+      <div className="w-full flex justify-between">
+        <h1 className="font-semibold text-xl">Todo App</h1>
+        <Link href="/new" className="rounded border border-gray-200 px-4 py-2">
+          Add New
+        </Link>
+      </div>
+      <ul className="w-full px-6 p-y2">
+        {res.map((res) => (
+          <li key={res.id} className="">
+            {res.title}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
